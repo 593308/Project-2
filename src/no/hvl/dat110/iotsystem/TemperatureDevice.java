fe308d002c47f9a59dd91ddc73ff4de34772c11a
Matias <h593308@stud.hvl.no>
2022-03-07 14:08:33 +0100

    first commit

--- src/no/hvl/dat110/iotsystem/TemperatureDevice.java
+++ src/no/hvl/dat110/iotsystem/TemperatureDevice.java
@@ -15,16 +15,32 @@ public class TemperatureDevice {
 		// TODO - start
 
 		// create a client object and use it to
+		
+		Client client = new Client("sensor", Common.BROKERHOST, Common.BROKERPORT);
+		
 
 		// - connect to the broker - user "sensor" as the user name
+		client.connect();
 		// - publish the temperature(s)
+		
+		for (int i = 0; i < COUNT; i++) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			client.publish(Common.TEMPTOPIC, "" + sn.read());
+		}
+		
 		// - disconnect from the broker
+		
+		client.disconnect();
 
 		// TODO - end
 
 		System.out.println("Temperature device stopping ... ");
 
-		throw new UnsupportedOperationException(TODO.method());
 
 	}
 }
